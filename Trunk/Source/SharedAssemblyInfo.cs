730b2dce3b989c784e1c09339ac7a0f50a51e050
chad2wick <github-wc167@blockworks.com>
2015-09-13 12:03:25 -0500

    This is the basic per user installer. Addin is registered and running in Outlook. Uninstall removes everything.

--- Trunk/Source/SharedAssemblyInfo.cs
+++ Trunk/Source/SharedAssemblyInfo.cs
@@ -27,14 +27,14 @@ using System.Runtime.InteropServices;
 
 // Note that the assembly version does not get incremented for every build to avoid problems with assembly binding (or
 // requiring a policy or <bindingRedirect> in the config file).
-[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyVersion("0.9.0.0")]
 
 // By default, the "Product version" shown in the file properties window is the same as the value specified for
 // AssemblyFileVersionAttribute. Set AssemblyInformationalVersionAttribute to be the same as AssemblyVersionAttribute so
 // that the "Product version" in the file properties window matches the version displayed in the GAC shell extension.
-[assembly: AssemblyInformationalVersion("1.1.0.0")] // a.k.a. "Product version"
+[assembly: AssemblyInformationalVersion("0.9.0.0")] // a.k.a. "Product version"
 
 // The AssemblyFileVersionAttribute is incremented with every build in order to distinguish one build from another.
 // AssemblyFileVersion is specified in AssemblyVersionInfo.cs so that it can be easily incremented by the automated
 // build process.
-[assembly: AssemblyFileVersion("1.1.432.0")]
+[assembly: AssemblyFileVersion("0.9.486.0")]
